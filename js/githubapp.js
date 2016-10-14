1bbae59a16e8568cea0eabbffc2ef4ab2f2d4a13
Marta Sanchez <sanchez.marta@gmail.com>
2016-10-14 09:36:22 -0700

    display followers

--- js/githubapp.js
+++ js/githubapp.js
@@ -4,11 +4,11 @@ function GitHubSearch() {
 
 }
 
-GitHubSearch.prototype.userLookup = function(username){
+GitHubSearch.prototype.userLookup = function(username, displayResults){
 
   $.get('https://api.github.com/users/' + username + '?access_token=' + apiKey).then(function(response){
   console.log(response);
-  displayResults(username, responseJSON.followers)
+  displayResults(username, response.followers)
   }).fail(function(error){
   console.log(error.responseJSON.message);
   });
